d607c81bc24d6cbdd09d714afcb41f265694dfd5
Sirli Kont <sirlikont@gmail.com>
2025-11-28 20:11:55 +0200

    ref funktsioon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
-
-let message = "Hello Vue 3 with Vite and Bun!";
-
+import { ref } from 'vue';
+let message = ref ("Hello Vue 3 with Vite and Bun!");
 </script>
 
-<template>
-    
-    <div class="container">
+<template>  
+    <div class="container content mt-5">
+        <button class="button is-primary is-light mt-5" v-on:click="message = 'Hello KTA24!'">A Bulma Button</button>
         <h1>{{ message }}</h1>
     </div>
-    
 </template>
