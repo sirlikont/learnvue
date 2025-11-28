57aec1ecace27716403301a951fd8279cc75082d
Sirli Kont <sirlikont@gmail.com>
2025-11-28 19:56:35 +0200

    bun, vue, bulma

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+let message = "Hello Vue 3 with Vite and Bun!";
+
+</script>
+
+<template>
+    
+    <div class="container">
+        <h1>{{ message }}</h1>
+    </div>
+    
+</template>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
