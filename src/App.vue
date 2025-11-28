d24823cb1fd42278cb8c27e39db2e557b50c0ffd
Sirli Kont <sirlikont@gmail.com>
2025-11-28 21:12:29 +0200

    poenimekiri

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <script setup>
 import { ref } from 'vue';
 
-let message = ref ("Hello Vue 3 with Vite and Bun!");
+let newItem = ref ("");
+let items = ref (["Piimm", "Leib", "Munad"]);
 
-
+function addItem() {
+    if (newItem.value.trim() !== "") {
+        items.value.push(newItem.value.trim());
+        newItem.value = "";
+    }
+}
 </script>
 
 <template>  
-    <div class="container content mt-5">
-        <button class="button is-primary is-light mt-5" @click="message = 'Hello KTA24!'">A Bulma Button</button>
-         <input class="input" v-model="message"/>
-        <h1>{{ message.split('').reverse().join('') }}</h1>
+    <div class="container is-fluid content mt-5">
+        <div class="field has-addons">
+            <div class="control is-expanded">
+                <input @keydown.enter="addItem" v-model="newItem" class="input" type="text" placeholder="Add item">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <h2>Poenimekiri:</h2>
+        <ul>
+            <li v-for="item in items">{{ item }}</li>
+        </ul>
     </div>
 </template>
