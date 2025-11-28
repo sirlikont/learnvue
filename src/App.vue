2e8a2701e383b463d008bff284720c35a508efe7
Sirli Kont <sirlikont@gmail.com>
2025-11-28 20:33:04 +0200

    event

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import { ref } from 'vue';
+
 let message = ref ("Hello Vue 3 with Vite and Bun!");
+
+function model(event) {
+   console.log(event);
+   message.value = event.target.value;
+}
 </script>
 
 <template>  
     <div class="container content mt-5">
-        <button class="button is-primary is-light mt-5" v-on:click="message = 'Hello KTA24!'">A Bulma Button</button>
-         <input class="input" v-model="message" />
+        <button class="button is-primary is-light mt-5" @click="message = 'Hello KTA24!'">A Bulma Button</button>
+         <input class="input" :value="message" @input="model"/>
         <h1>{{ message }}</h1>
     </div>
 </template>
