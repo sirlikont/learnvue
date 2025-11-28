22f0bffa6c4320bf7f9b9101d7a41845964ecef8
Sirli Kont <sirlikont@gmail.com>
2025-11-28 23:12:54 +0200

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,32 @@
 <script setup>
-import { ref, computed } from 'vue';
-import ItemList from './ItemList.vue';
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+import ImgModal from './components/ImgModal.vue';
 
-let newItem = ref ("");
-let i = 1;
-let items = ref ([
-    {id: i++, name: "Piim", isDone: false},
-    {id: i++, name: "Leib", isDone: true},
-    {id: i++, name: "Munad", isDone: false}
-]);
-
-function addItem() {
-    if (newItem.value.trim() !== "") {
-        items.value.push({id: i++, name: newItem.value.trim(), isDone: false});
-        newItem.value = "";
-    }
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let toDoItems = computed(() => items.value.filter(item => !item.isDone));
+let modalActive = ref(false);
+let modal2Active = ref(false);
+let modal3Active = ref(false);
 
 </script>
 
-<template>  
-    <div class="container is-fluid content mt-5">
-        <div class="field has-addons">
-            <div class="control is-expanded">
-                <input @keydown.enter="addItem" v-model="newItem" class="input" type="text" placeholder="Add item">
+<template>
+    <div class="container is-fluid content mt-2">
+         <button class="button is-primary" @click="modalActive = true">Open Modal</button>
+        <button class="button is-info" @click="modal2Active = true">Open Modal 2</button>
+        <button class="button is-warning" @click="modal3Active = true">Open Modal 3</button>
+        <ImgModal :active="modalActive" @close="modalActive = false" url="https://picsum.photos/1280/960?r=1"></ImgModal>
+        <ImgModal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?r=2"></ImgModal>
+        <Modal :active="modal3Active" @close="modal3Active = false">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec
+                        id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus
+                        et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis
+                        consectetur purus sit amet fermentum.
+                    </div>
+                </div>
             </div>
-            <div class="control">
-                <button class="button is-info" @click="addItem">
-                    Add item
-                </button>
-            </div>
-        </div>
-
-        <ItemList :items="items" title="Poenimekiri:"></ItemList>
-        <ItemList :items="doneItems" title="Ostetud:"></ItemList>
-        <ItemList :items="toDoItems" title="Ostmata:"></ItemList>
+        </Modal>
     </div>
 </template>
---
+++ src/pages/ToDo.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, computed } from 'vue';
+import ItemList from '../components/ItemList.vue';
+
+let newItem = ref ("");
+let i = 1;
+let items = ref ([
+    {id: i++, name: "Piim", isDone: false},
+    {id: i++, name: "Leib", isDone: true},
+    {id: i++, name: "Munad", isDone: false}
+]);
+
+function addItem() {
+    if (newItem.value.trim() !== "") {
+        items.value.push({id: i++, name: newItem.value.trim(), isDone: false});
+        newItem.value = "";
+    }
+}
+
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+let toDoItems = computed(() => items.value.filter(item => !item.isDone));
+
+</script>
+
+<template>  
+    <div class="container is-fluid content mt-5">
+        <div class="field has-addons">
+            <div class="control is-expanded">
+                <input @keydown.enter="addItem" v-model="newItem" class="input" type="text" placeholder="Add item">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+
+        <ItemList :items="items" title="Poenimekiri:"></ItemList>
+        <ItemList :items="doneItems" title="Ostetud:"></ItemList>
+        <ItemList :items="toDoItems" title="Ostmata:"></ItemList>
+    </div>
+</template>
