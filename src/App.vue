ef18380bcbf2cd350cea2178112c174ccdaa4875
Sirli Kont <sirlikont@gmail.com>
2025-11-28 20:22:06 +0200

    input

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ let message = ref ("Hello Vue 3 with Vite and Bun!");
 <template>  
     <div class="container content mt-5">
         <button class="button is-primary is-light mt-5" v-on:click="message = 'Hello KTA24!'">A Bulma Button</button>
+         <input class="input" v-model="message" />
         <h1>{{ message }}</h1>
     </div>
 </template>
