3fe61494f4e72c4fab501b163f4341f1a2e5ec37
Sirli Kont <sirlikont@gmail.com>
2025-12-12 20:06:42 +0200

    refactor: replace XMLHttpRequest with fetch for joke fetching

--- src/pages/ChuckNorris.vue
+++ src/pages/ChuckNorris.vue
@@ -3,15 +3,12 @@ import { ref } from 'vue';
 
 let joke = ref('');
 
-const req = new XMLHttpRequest();
-req.addEventListener("load", function (){
-    let res = this.responseText;
-    let data = JSON.parse(res);
-    console.log(data.value);
+fetch('https://api.chucknorris.io/jokes/random').then(res => {
+    return res.json();
+}).then(data => {
     joke.value = data.value;
 });
-req.open("GET", "https://api.chucknorris.io/jokes/random");
-req.send();
+
 </script>
 <template>
 <div class="container content">
