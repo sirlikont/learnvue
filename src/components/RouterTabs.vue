eccc5387293f9dd6dc8ace66179866ce59fc7936
Sirli Kont <sirlikont@gmail.com>
2025-12-12 19:41:13 +0200

    refactor: update RouterTabs to dynamically generate tabs from routes

--- src/components/RouterTabs.vue
+++ src/components/RouterTabs.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { useRoute, useRouter, RouterLink } from 'vue-router';
 
+const router = useRouter();
+const route = useRoute();
 </script>
 <template>
     <div class="tabs is-centered">
         <ul>
-            <li><RouterLink to="/">ToDo</RouterLink></li>
-            <li><RouterLink to="/modals">Modals</RouterLink></li>
+            <li v-for="route in $router.getRoutes()" :class="{'is-active': route.path === $route.path}">
+                <RouterLink :to="route.path">{{ route.name }}</RouterLink>
+            </li>
         </ul>
     </div>
 </template>
