35aa6b33a7fdf9e7250ab2d0dbce94501caa0b29
Sirli Kont <sirlikont@gmail.com>
2025-12-12 20:05:23 +0200

    feat: add ChuckNorris page with joke fetching functionality

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { createMemoryHistory, createWebHashHistory, createWebHistory, createRout
 
 import ToDo from './pages/ToDo.vue'
 import Modals from './pages/Modals.vue'
+import ChuckNorris from './pages/ChuckNorris.vue';
 
 const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals' },
+  { path: '/chuck', component: ChuckNorris, name: 'ChuckNorris' },
 ]
 
 export const router = createRouter({
---
+++ src/pages/ChuckNorris.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+let joke = ref('');
+
+const req = new XMLHttpRequest();
+req.addEventListener("load", function (){
+    let res = this.responseText;
+    let data = JSON.parse(res);
+    console.log(data.value);
+    joke.value = data.value;
+});
+req.open("GET", "https://api.chucknorris.io/jokes/random");
+req.send();
+</script>
+<template>
+<div class="container content">
+    <h1>{{ joke }}</h1>
+</div>
+</template>
